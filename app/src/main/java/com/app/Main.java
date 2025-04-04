840e8feb6ddf330e5e9d041b63b6551c9f49ab38
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2025-04-04 16:16:06 -0400

    change commit

--- app/src/main/java/com/app/Main.java
+++ app/src/main/java/com/app/Main.java
@@ -11,13 +11,9 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        Lista l1 = new Lista();
         Lista l2 = new Lista();
-        // l2.pasarDigitos(1245213);
-        l1.pasarDigitos(1213411);
-        // System.out.println(l1.mismosElem(l2));
-        System.out.println(l1);
-        l1.eliminarTodo(1);
-        System.out.println(l1);
+        l2.pasarDigitos(12);
+        System.out.println(l2);
+        System.out.println(l2.suma());
     }
 }
